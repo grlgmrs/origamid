2046ddabd30987ad4f7816e98d418eae82f09c80
Gabriel Guimaraes <gabriel.ctiunesp@gmail.com>
2020-09-01 15:26:29 -0300

    Vue.js Completo: Aula 0405

--- Vuejs Completo/projetos/techno/app.js
+++ Vuejs Completo/projetos/techno/app.js
@@ -2,6 +2,7 @@ const vm = new Vue({
   el: "#app",
   data: {
     products: {},
+    product: false,
   },
   filters: {
     formatPrice(v) {
@@ -17,6 +18,13 @@ const vm = new Vue({
         .then((r) => r.json())
         .then((r) => {
           this.products = r;
+        });
+    },
+    fetchProduct(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => {
+          this.product = r;
         });
     },
   },
